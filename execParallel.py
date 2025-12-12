11bde9b9b0e9594af679e8e6db40c61971003a22
Serginho <gabrieldiashe@gmail.com>
2025-12-12 19:57:28 -0300

    fix: concerto de alguns bugs

--- execParallel.py
+++ execParallel.py
@@ -1,9 +1,10 @@
 import subprocess as sp
 import multiprocessing as mp
+import time
 from typing import List, Tuple
 
 def prepareFileDestiny(origin:str, destiny:str, metricType) -> None:
-    fileWithExtension:str = path.split("/")[-1]
+    fileWithExtension:str = origin.split("/")[-1]
     fileName:str = fileWithExtension.split(".")[0]
     path:str = destiny + fileName + "." + metricType
     return path
@@ -12,21 +13,21 @@ def createFile(path:str,content:str) -> None:
     with open(f"{path}","w") as file:
         file.write(content)
 
-def cronometrarSubprocess(path:str, cmd:str)
+def cronometrarSubprocess(path:str, cmd:str) -> float:
     command:List = []
     if cmd == "./":
         command = [f"./{cmd}"]
     else:
-        command = [cmd,file]
+        command = [cmd,path]
 
-    start = time.start()
+    start = time.time()
     sp.run(command)
     end = time.time() - start
 
     return end
 
 def prepareContent(totalTime):
-    content = f"segundos: {totalTime:.4f}\n"
+    return f"segundos: {totalTime:.4f}\n"
 
 #por falta de nome melhor
 #vai obter o tempo, preparar o path de destino, preparar o conteúdo do arquivo e salvar
@@ -51,8 +52,21 @@ def execBatch(programs:List[Tuple[str,str,str]], cpuTime=False, realTime=False) 
     for p in process:
         p.join()
 
+"""
+Uma outra funcionalidade legal é implementar os algoritmos no próprio arquivo principal
+e depois especificar na biblioteca que se trata de uma leitura "local". Notei que ter
+que especificar o path dos arquivos pode ser meio incoviniente para programas pequenos.
+
+Ou também ser possível combinar funções localmente e comparar com os arquivos, mas uma
+coisa de cada vez.
+"""
 def main():
-    pass
+    programs = [
+        ("codigosTeste/triviais/script1.py","codigosTeste/triviais/outputs", "python3"),
+        ("codigosTeste/triviais/script2.js","codigosTeste/triviais/outputs", "node"),
+        ("codigosTeste/triviais/programa1.out","codigosTeste/triviais/outputs","./")
+        ]
+    execBatch(programs)
 
 if __name__ == "__main__":
     main()
